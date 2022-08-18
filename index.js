6882317517410ebd00ee20c2d1cb37ded0b44621
janekigenyi <janekigenyi@gmail.com>
2022-08-18 08:33:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    //returns the number of blocks given a value
+    if (distance<42){
+      return(42-distance)
+    }
+    else {
+  
+    return(distance-42);
+  }
+  }
+
+  function distanceFromHqInFeet (distance) {
+    return distanceFromHqInBlocks(distance)*264;
+  }
+  function distanceTravelledInFeet(y,z){
+    if (b>a){
+      return((z-y)*264);
+    }
+    else
+      return((y-z)*264);
+  }
+  
